9b45346230ee0311ec366c16f525c38e637e4036
Danielvas96 <daniel.vasconcelos@madeiramadeira.com.br>
2024-02-22 11:39:39 -0300

    incluido o coverage

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    collectCoverage: true,
+  
+    coverageReporters: ['lcov', 'text-summary'],
+
+    coverageThreshold: {
+        global: {
+          statements: 75,
+          branches: 75,
+          functions: 75,
+          lines: 75,
+        },
+      },
+
+
+  }
