43b55b2f36f4ebe59052bd4839a13afd63596414
Danielvas96 <daniel.vasconcelos@madeiramadeira.com.br>
2024-02-23 09:49:50 -0300

    teste

--- Calculadora.test.js
+++ Calculadora.test.js
@@ -9,9 +9,9 @@ test('validar subtração', () => {
     expect(subtracao(10, 5)).toBe(5)
 })
 
-test('validar multiplicação', () => {
-    expect(multiplicacao(3, 4)).toBe(12)
-})
+// test('validar multiplicação', () => {
+//     expect(multiplicacao(3, 4)).toBe(12)
+// })
 
 // test('validar divisão', () => {
 //     expect(divisao(10, 2)).toBe(5)
