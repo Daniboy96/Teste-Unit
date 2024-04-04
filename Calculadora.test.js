f243983da12e7fac1c585955cf0f8d68c3b1dbd4
Danielvas96 <daniel.vasconcelos@madeiramadeira.com.br>
2024-04-04 14:04:39 -0300

    Ajuste

--- Calculadora.test.js
+++ Calculadora.test.js
@@ -9,10 +9,10 @@ test('validar subtração', () => {
     expect(subtracao(10, 5)).toBe(5)
 })
 
-// test('validar multiplicação', () => {
-//     expect(multiplicacao(3, 4)).toBe(12)
-// })
+test('validar multiplicação', () => {
+    expect(multiplicacao(3, 4)).toBe(12)
+})
 
-// test('validar divisão', () => {
-//     expect(divisao(10, 2)).toBe(5)
-// })
+test('validar divisão', () => {
+    expect(divisao(10, 2)).toBe(5)
+})
